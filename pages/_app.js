bc8be65ee2d885c28438d030e01681fd4fc01b94
KJH <picsmash7@gmail.com>
2023-01-30 03:59:35 +0900

    feat/theme

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
     return(
-        <ThemeProvider attribute="class" enableSystem={false}>
+        <ThemeProvider attribute="class">
             <Component {...pageProps} />
         </ThemeProvider>
     );
